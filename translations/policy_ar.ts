da0ba7167f8eb2664b56252166407f73e35c2a09
Ahmad Nourallah <ahmadnurallah@gmail.com>
2017-10-19 23:18:14 +0300

    Create and finish arabic translation

---
+++ translations/policy_ar.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+	<context>
+		<name>policy</name>
+		<message>
+			<location filename="com.deepin.pkexec.deepin-clone!message" line="0" />
+			<source>Password required to run Deepin Clone</source>
+			<translation type="finished">كلمة المرور مطلوبة لتشغيل ديبين كلون</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.pkexec.deepin-clone!description" line="0" />
+			<source>Deepin Clone needs to do operations on block device, such as write and read, get info and etc.</source>
+			<translation type="finished">ديبين كلون يحتاج للقيام بعمليات على جهاز الكتلة, مثل الكتابة والقراءة و الحصول على المعلومات ..إلخ.</translation>
+		</message>
+	</context>
+</TS>
